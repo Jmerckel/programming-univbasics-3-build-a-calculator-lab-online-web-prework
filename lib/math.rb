4e3ec67693fdb57f57f8930f5af1147b317b9f73
Jazmin M <jazminmerckel@gmail.com>
2019-07-31 18:13:29 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,26 +1,22 @@
 def addition(num1, num2)
   num1 + num2
 end
-addition(1, 2)
 
 def subtraction(num1, num2)
   num1 - num2
 end
-subtraction(1, 2)
 
 def division(num1, num2)
-  divide = 10 / 2
+  num1 / num2
 end
 
 def multiplication(num1, num2)
-  multiply = 10 * 8
+  num1 * num2
 end
-multiple = 
 
 def modulo(num1, num2)
   num1 % num2
 end
-modulo(1, 2)
 
 def square_root(num)
   Math.sqrt(num)
