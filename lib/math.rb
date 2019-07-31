977c0a9d5d02c1fa5e62ce42004ac20698509948
Jazmin M <jazminmerckel@gmail.com>
2019-07-31 17:56:09 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,24 +1,29 @@
 def addition(num1, num2)
-  sum = 5 + 4
+  add = 5 + 4
 end
-sum
+add
 
 def subtraction(num1, num2)
-
+  subtract = 9 - 8
 end
+subtract 
 
 def division(num1, num2)
-
+  divide = 10 / 2
 end
+divide
 
 def multiplication(num1, num2)
-
+  multiply = 10 * 8
 end
+multiply
 
 def modulo(num1, num2)
-
+  modulo = 10 ^ 2
 end
+modulo
 
 def square_root(num)
-
+  square = 4**3
 end
+square
